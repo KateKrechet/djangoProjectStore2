96080b8db5fd9d443be64aff95931be769a3c393
KateKrechet <k.krechet@yandex.ru>
2023-11-26 21:58:14 +0300

    make product_list, product_list_detail

--- djangoProjectStore/urls.py
+++ djangoProjectStore/urls.py
@@ -20,5 +20,10 @@ from store import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index,name='home'),
+    path('', views.product_list, name='home'),
+    path('<slug:category_slug>/', views.product_list,
+         name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_detail,
+         name='product_detail'),
+
 ]
--- store/models.py
+++ store/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=200,verbose_name = 'Категория')
+    name = models.CharField(max_length=200, verbose_name='Категория')
     slug = models.SlugField(max_length=200)
 
     class Meta:
@@ -13,6 +14,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('product_list_by_category', args=[self.slug])
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name='Категория')
@@ -21,7 +25,7 @@ class Product(models.Model):
     image = models.CharField(max_length=100, blank=True, null=True, verbose_name='Картинка')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
-    volume = models.CharField(max_length=50,null=True, verbose_name='Объем')
+    volume = models.CharField(max_length=50, null=True, verbose_name='Объем')
     available = models.BooleanField(default=True, verbose_name='Доступность товара')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
@@ -33,3 +37,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.id, self.slug])
