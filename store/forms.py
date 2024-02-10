83cf950c5874ed8787fcc454d3791731d507edc5
KateKrechet <k.krechet@yandex.ru>
2024-02-10 20:55:07 +0300

    make little design

--- store/forms.py
+++ store/forms.py
@@ -5,15 +5,15 @@ from django.forms import TextInput
 from .models import *
 
 class Signupform(UserCreationForm):
-    username = forms.CharField(label='логин', help_text='')
-    password1 = forms.CharField(label='пароль', help_text='',
+    username = forms.CharField(label='Логин', help_text='')
+    password1 = forms.CharField(label='Пароль', help_text='',
                                 widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}))
-    password2 = forms.CharField(label='подтверждение', help_text='',
+    password2 = forms.CharField(label='Подтверждение', help_text='',
                                 widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}))
 
-    email = forms.EmailField(label='почта', widget=forms.TextInput(attrs={'placeholder': 'qwe@mail.ru'}))
-    first_name = forms.CharField(label='имя', max_length=20)
-    last_name = forms.CharField(label='фамилия', max_length=20, required=False)
+    email = forms.EmailField(label='Email', widget=forms.TextInput(attrs={'placeholder': 'qwe@mail.ru'}))
+    first_name = forms.CharField(label='Имя', max_length=20)
+    last_name = forms.CharField(label='Фамилия', max_length=20, required=False)
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 11)]
 class CartAddProductForm(forms.Form):
